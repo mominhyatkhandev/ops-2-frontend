644f3183efb4d11a812954c7eb2e8a7aab1aaf75
Momin Hayat Khan <mominhyatkhandeveloper@gmail.com>
2023-11-29 13:10:33 +0500

    padding miniapps section

--- src/components/Content/MiniApps/MiniApps.tsx
+++ src/components/Content/MiniApps/MiniApps.tsx
@@ -6,7 +6,7 @@ const MiniApps = (): JSX.Element => {
   return (
     <div id="NewRootRoot" className="flex flex-col w-full mb-[20px]">
       {/* <div className="ma-bg bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-col justify-center min-h-[640px] shrink-0 items-center"> */}
-      <div className="min-h-[640px] flex flex-col justify-center shrink-0 items-center relative">
+      <div className="min-h-[640px] flex flex-col justify-center shrink-0 px-[254px] py-[156px] items-center relative">
         <Image
           src={miniApps}
           alt="miniApps Background"
@@ -15,7 +15,7 @@ const MiniApps = (): JSX.Element => {
           quality={100}
           className="absolute inset-0"
         />
-        <div className="shadow-[0px_4px_16px_0px_rgba(51,_51,_51,_0.08)] bg-neutral-white-base flex flex-col justify-center gap-16 w-2/3 h-auto shrink-0 items-center p-[40px] rounded-lg relative">
+        <div className="shadow-[0px_4px_16px_0px_rgba(51,_51,_51,_0.08)] bg-neutral-white-base flex flex-col justify-center gap-16 shrink-0 items-center p-10 rounded-lg relative">
           <div
             id="MiniApps1"
             className="text-5xl font-semibold leading-[60px] text-secondary-base"
